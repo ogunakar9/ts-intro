e9729ee6689870bc1716c994b35d12acdd2e242f
Ogun Akar <ogun.akar@csadmins-MacBook-Pro.local>
2022-01-04 09:15:31 +0300

    ts practice init

---
+++ app.js
@@ -0,0 +1,6 @@
+function addNums(number1, number2) {
+    return number1 + number2;
+}
+var n1 = 5;
+var n2 = 2.8;
+console.log(addNums(n1, n2));
---
+++ app.ts
@@ -0,0 +1,8 @@
+function addNums(number1: number, number2: number) {
+  return number1 + number2;
+}
+
+const n1 = 5;
+const n2 = 2.8;
+
+console.log(addNums(n1, n2));
